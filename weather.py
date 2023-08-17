f71610f0a502c2c9bf61be0d34c576436da8ebdd
unknown <congwork2023@gmail.com>
2023-08-17 10:33:13 +0700

    add comment

--- weather.py
+++ weather.py
@@ -6,14 +6,15 @@ from PIL import ImageTk, Image
 
 
 url ="https://weather.com/en-CA/weather/today/l/c4877d013a65b996c469bc919c35c1b1af5cb13c0c6868275de92ba1e2a936cb7e6c9b3cf58bb491f3a1347e4d84b14e"
+# Với cách này là lấy URL chưa phải là API
 
-master=Tk()
+master=Tk() # tạo cửa sổ window
 master.title("Weather App")
-master.config(bg="white")
+master.config(bg="white") #
 
-img=Image.open("Img\weather_logo.png")
-img =img.resize((150,150))
-img=ImageTk.PhotoImage(img)
+img=Image.open("Img\weather_logo.png") #nằm trong module PIL
+img =img.resize((150,150))# NẰM TRONG PIL -> thay đổi kích thước ảnh
+img=ImageTk.PhotoImage(img) #add vào widget 
 
 def getWeather():
     page = requests.get(url)
@@ -35,8 +36,9 @@ locationLabel.grid(row=0,sticky="N", padx=100)
 
 temperatureLabel = Label(master,font=("Calibri Bold",70),bg="white")
 temperatureLabel.grid(row=1,sticky="W", padx=40)
-Label(master, image=img, bg="white").grid(row=1,sticky="E")
+Label(master, image=img, bg="white").grid(row=1,sticky="E") #######cái này để hiển thị logo image
 weatherPredictionLabel = Label(master,font=("Calibri bold", 51),bg="white")
 weatherPredictionLabel.grid(row=2, sticky="W",padx=40)
+
 getWeather()
 master.mainloop()
