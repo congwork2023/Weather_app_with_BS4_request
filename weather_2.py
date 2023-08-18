8da06232f063c6ff9546183889e134833e9a8710
unknown <congwork2023@gmail.com>
2023-08-18 14:48:50 +0700

    replace master variable to window

--- weather_2.py
+++ weather_2.py
@@ -3,9 +3,9 @@ from tkinter import Label
 from PIL import Image, ImageTk # position of logo
 
 
-master=Tk() #make window
-master.title("weather_app") #create title
-master.config(bg="white") #Configuration background is white
+window=Tk() #make window (replace master variable to window)
+window.title("weather_app") #create title
+window.config(bg="white") #Configuration background is white
 
 #position logo
 img=Image.open("Img\weather_logo.png")
@@ -14,14 +14,14 @@ img=img.resize((150,150))
 #add Imamge into object
 img_Object =ImageTk.PhotoImage(img)
 #create Lable and position of Lable by grid
-Label(master, image=img_Object, bg="white").grid(row=1,sticky="E")
+Label(window, image=img_Object, bg="white").grid(row=1,sticky="E")
 
 
-locationLable=Label(master,font=("Calibri Bold",20),bg="white") #tạo nhãn cho các biến 
-temperatureLable= Label(master,font=("Calibri Bold",70),bg="white")#như trên# Font lớn nhất 
-weatherprectionLable=Label(master,font=("Calibri Bold",50),bg="white")# như trên#
+locationLable=Label(window,font=("Calibri Bold",20),bg="white") #tạo nhãn cho các biến 
+temperatureLable= Label(window,font=("Calibri Bold",70),bg="white")#như trên# Font lớn nhất 
+weatherprectionLable=Label(window,font=("Calibri Bold",50),bg="white")# như trên#
 
 
 
 
-master.mainloop() #run the window
+window.mainloop() #run the window
