e1521d82da44db363a0f81fdde7e61c0ca68b1ea
unknown <congwork2023@gmail.com>
2023-08-18 14:42:57 +0700

    position logo

--- weather_2.py
+++ weather_2.py
@@ -1,11 +1,21 @@
 from tkinter import Tk
 from tkinter import Label
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk # position of logo
 
 
 master=Tk() #make window
 master.title("weather_app") #create title
 master.config(bg="white") #Configuration background is white
+
+#position logo
+img=Image.open("Img\weather_logo.png")
+img=img.resize((150,150))
+
+#add Imamge into object
+img_Object =ImageTk.PhotoImage(img)
+#create Lable and position of Lable by grid
+Label(master, image=img_Object, bg="white").grid(row=1,sticky="E")
+
 
 locationLable=Label(master,font=("Calibri Bold",20),bg="white") #tạo nhãn cho các biến 
 temperatureLable= Label(master,font=("Calibri Bold",70),bg="white")#như trên# Font lớn nhất 
